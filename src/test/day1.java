10d650d21c619f9c06e6e4409764555c35d97c80
sneha <snehanatekar@whitehatjr.com>
2021-06-18 21:28:09 +0530

    18th june test

--- src/test/day1.java
+++ src/test/day1.java
@@ -1,11 +1,13 @@
 package test;
 
+import org.testng.annotations.Test;
+
 public class day1 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	@Test
+	public void Demo() {
 
-		
+	System.out.println("Hello");	
 		
 	}
 
