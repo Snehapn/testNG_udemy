323fb475315839b5cd9da81a0a397ccd3fe31768
sneha <snehanatekar@whitehatjr.com>
2021-06-19 20:41:51 +0530

    testNG 19th june

--- src/test/day3.java
+++ src/test/day3.java
@@ -1,6 +1,7 @@
 package test;
 
 import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class day3 {
@@ -13,17 +14,27 @@ public class day3 {
 		
 	}
 	
-	@Test
+	@Test (groups = {"smoke"})
 	public void MobileLoginCarLoan() {
 
 	System.out.println("MobileLoginCarLoan");	
 		
 	}
 	
-	@Test
+	@Test (groups = {"smoke"})
 	public void LoginAPICarLoan() {
 
 	System.out.println("LoginAPICarLoan");	
+		
+	
+	}
+	
+	@Test (dataProvider = "getData")
+	public void LoginAPICarLoanTest (String username, String password) {
+
+	System.out.println("LoginAPICarLoanTest");	
+	System.out.println("username");	
+	System.out.println("password");	
 		
 	
 	}
@@ -33,5 +44,23 @@ public class day3 {
 
 	System.out.println("before_suite");	
 		
+	}
+	
+	
+	@DataProvider
+	public Object[][] getData() {
+		
+		Object[][] data = new Object [3][2];
+		
+		data[0][0] = "first_user";
+		data[0][1] = "first_password";
+		
+		data[1][0] = "second_user";
+		data[1][1] = "second_password";
+		
+		data[2][0] = "third_user";
+		data[2][1] = "third_password";
+		
+		return data;
 	}
 }
--- src/test/day4.java
+++ src/test/day4.java
@@ -1,25 +1,28 @@
 package test;
 
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class day4 {
 
 	
-	@Test
+	@Test (enabled = false)
 	public void WebLoginHomeLoan() {
 
 	System.out.println("WebLoginHomeLoan");	
 		
 	}
 	
+	@Parameters({"URL"})
 	@Test
-	public void MobileLoginHomeLoan() {
+	public void MobileLoginHomeLoan(String urlname) {
 
 	System.out.println("MobileLoginHomeLoan");	
+	System.out.println(urlname);	
 		
 	}
 	
-	@Test
+	@Test (dependsOnMethods = {"MobileLoginHomeLoan"})
 	public void LoginAPIHomeLoan() {
 
 	System.out.println("LoginAPIHomeLoan");	
