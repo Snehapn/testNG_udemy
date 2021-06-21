bef03ecf21665ca0c43f3859d29a70757bddc9a9
sneha <snehanatekar@whitehatjr.com>
2021-06-21 21:18:24 +0530

    change in folder

--- src/test/GlobalValuesDrive.java
+++ src/test/GlobalValuesDrive.java
@@ -11,14 +11,14 @@ public class GlobalValuesDrive {
 	public static void main (String [] args) throws IOException {
 		
 		Properties prop = new Properties();
-		FileInputStream fis = new FileInputStream("C:\\Users\\HP\\TestNG_udemy\\src\\test\\data.properties");
+		FileInputStream fis = new FileInputStream("D:\\udemy_courses\\TestNG_udemy\\src\\test\\data.properties");
 		prop.load(fis);
 		System.out.println(prop.getProperty("browser"));
 		System.out.println(prop.getProperty("url"));
 		
 		prop.setProperty("browser","firefox");
 		System.out.println(prop.getProperty("browser"));
-		FileOutputStream fos = new FileOutputStream("C:\\Users\\HP\\TestNG_udemy\\src\\test\\data.properties");
+		FileOutputStream fos = new FileOutputStream("D:\\udemy_courses\\TestNG_udemy\\src\\test\\data.properties");
 		prop.store(fos, null);
 
 	}
